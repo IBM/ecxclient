0922251b77aab672e2a710fa7cef1d2e65637fce
Ben Jones <benjones917@gmail.com>
2017-07-26 10:04:23 -0400

    update to allow for backup copy name matching in orarestorecopyversion

--- samples/orarestorecopyversion.py
+++ samples/orarestorecopyversion.py
@@ -2,8 +2,10 @@
 # Script to do an Oracle restore given an optional date range of the copy
 # An existing restore job is required, copy version of that job will be updated if applicable
 # If no date range is provided the latest copy will be used
+# Backup job name for the copy to use can also be provided
 # Set the cancel parameter = true if using this script to cancel/clean up an existing restore job
-#
+# The dest parameter is optional and will output logs at the end of the job if provided
+# 
 
 import json
 import sys
@@ -24,8 +26,12 @@ parser.add_option("--host", dest="host", help="ECX Host, (ex. https://172.20.58.
 parser.add_option("--restore", dest="restore", help="Restore Job Name")
 parser.add_option("--start", dest="start", help="Start Date filter for backup version (optional)")
 parser.add_option("--end", dest="end", help="End Date filter for backup version (optional)")
+parser.add_option("--backup", dest="backup", help="Backup job name for copy to use (optional)")
 parser.add_option("--cancel", dest="cancel", help="Enter 'true' for Cancel/Cleanup restore (optional)")
+parser.add_option("--dest", dest="dest", help="Destination to save log .csv (ex. /tmp/log.csv) (optional)")
 (options, args) = parser.parse_args()
+if (options.cancel is None):
+    options.cancel = "false"
 
 def prettyprint(indata):
     print json.dumps(indata, sort_keys=True,indent=4, separators=(',', ': '))
@@ -48,16 +54,47 @@ def get_version_for_policy(policy):
     metadata = {}
     sourceurl = policy['spec']['source'][0]['href']
     source = client.EcxAPI(session, 'oracle').get(url=sourceurl)
-    if (options.end is None or options.start is None):
+    # no backup filters supplied use latest
+    if (options.end is None and options.start is None and options.backup is None):
         version['href'] = source['links']['latestversion']['href']
         metadata['id'] = "latest"
         metadata['name'] = "Use Latest"
         version['metadata'] = metadata
         logger.info("Using latest backup copy version.")
         return version
+    # match on backup copy name no dates supplied
+    elif (options.end is None and options.start is None and options.backup is not None):
+        versionsurl = source['links']['versions']['href']
+        versions = client.EcxAPI(session, 'oracle').get(url=versionsurl)['versions']
+        for vers in versions:
+            prottime = int(vers['protectionInfo']['protectionTime'])
+            if (options.backup.upper() == vers['protectionInfo']['policyName'].upper()):
+                version['href'] = vers['links']['self']['href']
+                metadata['id'] = vers['id']
+                metadata['name'] = time.ctime(prottime/1000)[4:]
+                version['metadata'] = metadata
+                logger.info("Using backup copy version from: %s" % metadata['name'])
+                return version
+    # match on dates and backup copy name
+    elif (options.end is not None and options.start is not None and options.backup is not None):
+        start = int(datetime.datetime.strptime(options.start, '%m/%d/%Y %M:%S').strftime("%s"))*1000
+        end = int(datetime.datetime.strptime(options.end, '%m/%d/%Y %M:%S').strftime("%s"))*1000
+        versionsurl = source['links']['versions']['href']
+        versions = client.EcxAPI(session, 'oracle').get(url=versionsurl)['versions']
+        for vers in versions:
+            prottime = int(vers['protectionInfo']['protectionTime'])
+            if (prottime > start and prottime < end and options.backup.upper() == vers['protectionInfo']['policyName'].upper()):
+                version['href'] = vers['links']['self']['href']
+                metadata['id'] = vers['id']
+                metadata['name'] = time.ctime(prottime/1000)[4:]
+                version['metadata'] = metadata
+                logger.info("Using backup copy version from: %s" % metadata['name'])
+                prettyprint(vers)
+                return version
+    # match on dates no copy named supplied    
     else:
-        start = int(datetime.datetime.strptime(options.start, '%m/%d/%Y').strftime("%s"))*1000
-        end = int(datetime.datetime.strptime(options.end, '%m/%d/%Y').strftime("%s"))*1000
+        start = int(datetime.datetime.strptime(options.start, '%m/%d/%Y %M:%S').strftime("%s"))*1000
+        end = int(datetime.datetime.strptime(options.end, '%m/%d/%Y %M:%S').strftime("%s"))*1000
         versionsurl = source['links']['versions']['href']
         versions = client.EcxAPI(session, 'oracle').get(url=versionsurl)['versions']
         for vers in versions:
@@ -69,7 +106,7 @@ def get_version_for_policy(policy):
                 version['metadata'] = metadata
                 logger.info("Using backup copy version from: %s" % metadata['name'])
                 return version
-    logger.info("No backup copy found with provided dates")
+    logger.info("No backup copy found with provided dates or backup copy name")
     session.delete('endeavour/session/')
     sys.exit(2)
 
@@ -102,17 +139,18 @@ def cancel_restore_job(jobsession):
 
 def run_restore_job(job):
     logger.info("Running restore job: %s" % job['name'])
-    return client.JobAPI(session).run(job['id'])
+    job = client.JobAPI(session).run(job['id'])
 
 def run_restore():
     job = get_restore_job()
-    if (options.cancel is not None):
+    if (options.cancel.upper() == "TRUE"):
         jobsession = get_pending_job_session(job)
         cancel_restore_job(jobsession)
     else:
         policy = get_policy_for_job(job)
         version = get_version_for_policy(policy)
         policy['spec']['source'][0]['version'] = version
+        prettyprint(version)
         policy = update_restore_policy(policy)
         run_restore_job(job)
 
